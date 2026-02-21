1998ba840c1f452221831ea93b51b4e408a1a4c8
Anjelica(Anjie-MF) <anjiemay23@penguin>
2026-02-21 13:14:13 -0600

    Fix:validate input in button EH; update functionName for logic

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const guessedLettersElement = document.querySelector(".guessed-letters"); //UL for guessed letters
 const guessButton = document.querySelector(".guess");
-const textInput = document.querySelector(".letter"); //input box where player types a letter
+const playerGuess = document.querySelector(".letter"); //input box where player types a letter
 const wordInProgressElement = document.querySelector(".word-in-progress"); // display revealed letters/ blanks
 const remainingGuessesContainer = document.querySelector(".remaining"); //container for remaning guesses
 const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
-const keyboard = document.querySelector(".keyboard");
-const museCardContainer = document.querySelector(".muse-card");
-const museSigilElement = document.querySelector(".muse-sigil");
-const museNameElement = document.querySelector(".muse-name");
-const museHintElement = document.querySelector(".muse-hint");
+// const keyboard = document.querySelector(".keyboard");
+// const museCardContainer = document.querySelector(".muse-card");
+// const museSigilElement = document.querySelector(".muse-sigil");
+// const museNameElement = document.querySelector(".muse-name");
+// const museHintElement = document.querySelector(".muse-hint");
 const word = "grimoire";
 
 const placeholder = function (word) { //allows function to see the secret word
@@ -17,29 +18,34 @@ const placeholder = function (word) { //allows function to see the secret word
 
     for (const letter of word) {
         console.log(letter);
-        placeholderArray.push("🕯️");
+        placeholderArray.push("🔮");
     }
     wordInProgressElement.innerText = placeholderArray.join("");
 }
 placeholder(word);
 
+
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const inputValueContainer = textInput.value; //get the letter out of the mailbox
+    message.innerText = "";
+    const inputValueContainer = playerGuess.value; //get the letter out of the mailbox
     console.log(inputValueContainer); //show me the letter
-    inputValueContainer.value = ""; //the inbox for tomorrow mail 
+    inputValueContainer.value = ""; //clear the mailbox for tomorrow's mail
+
+    checkPlayerInput(inputValueContainer);
 })
 
-const checkPlayerInput = function (textInput) {
+
+const checkPlayerInput = function (playerGuess) {
     const acceptedLetter = /[a-zA-Z]/;
 
-    if (textInput === '') {
-        console.log("You can't leave the offering empty, Mortal.")
-    } else if (textInput.length > 1) {
-        console.log("You can only submit one offering at a time, Mortal.")
-    } else if (textInput.match(/[a-zA-Z]/)) {
-        console.log("Your offering doesn't meet the required standard, Mortal.")
+    if (playerGuess === '') {
+        message.innerText = "You can't leave the offering empty, Mortal.";
+    } else if (playerGuess.length > 1) {
+        message.innerText = "You can only submit one offering at a time, Mortal.";
+    } else if (!playerGuess.match(acceptedLetter)) {
+        message.innerText = "Your offering doesn't meet the required standard, Mortal.";
     } else {
-        return textInput
+        return playerGuess;
     }
 }
